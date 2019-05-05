2817efb38c904c2239e9a850df569f4d016c4a4f
morgoton <42516049+morgoton@users.noreply.github.com>
2019-05-05 20:27:51 +0300

    Delete query from 3 tables.sql

--- query from 3 tables.sql
+++
@@ -1,4 +0,0 @@
-select cities.city, cities.datetime as alltime,regions.region, regions.time, countries.country, countries.date from cities 
-left outer join regions on (cities.id_region = regions.id) 
-left outer join countries on (regions.id_country = countries.id)
-order by datetime asc;
