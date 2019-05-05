8127c065d6cef5c3c98bdbbf5691f0f7f7016546
morgoton <42516049+morgoton@users.noreply.github.com>
2019-05-05 20:26:58 +0300

    Delete Cities.sql

--- Cities.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists cities
-(id integer not null primary key auto_increment,
-city varchar(100) not null unique,
-id_region int not null,
-foreign key (id_region) references regions(id))
-Engine InnoDB;
