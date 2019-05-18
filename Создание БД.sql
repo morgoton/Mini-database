22f0370c47f5e8cacee49c71a39a8b39f0639f56
morgoton <42516049+morgoton@users.noreply.github.com>
2019-05-18 11:09:49 +0300

    Add files via upload

---
+++ Создание БД.sql
@@ -0,0 +1,9 @@
+-- show databases;
+-- create database if not exists connection character set utf8 collate utf8_general_ci;
+-- show databases;
+-- drop database if exists connection;
+-- show databases;
+-- create database if not exists connection character set utf8 collate utf8_general_ci;
+-- select default_character_set_name, default_collation_name from information_schema.schemata where schema_name = 'connection';
+-- alter database connection character set utf8mb4 collate utf8mb4_general_ci;
+-- select default_character_set_name, default_collation_name from information_schema.schemata where schema_name = 'connection';
