80350264cd8e1e604b7f39478da2e291b40aa4b2
morgoton <42516049+morgoton@users.noreply.github.com>
2019-05-19 10:56:11 +0300

    Add files via upload

---
+++ Наполнение таблиц данными.sql
@@ -0,0 +1,15 @@
+-- insert into countries (country) values ('Россия');
+-- insert into countries (country) values ('USA');
+-- insert into countries (country) values ('France');
+-- select * from countries;
+
+-- insert into regions (region, time, id_country) values ('ILL De france', '12:00:00', '3');
+-- insert into regions (region, time, id_country) values ('State New York', '02:00:00', '2');
+-- insert into regions (region, time, id_country) values ('Moscow region', '16:00:00', '1');
+-- select * from regions;
+
+-- insert into cities (city,id_region) values ('Moscow','1');
+-- insert into cities (city,id_region) values ('New York','2');
+-- insert into cities (city,id_region) values ('Zelenograd','1');
+-- insert into cities (city,id_region) values ('Paris','3');
+-- select * from cities;
