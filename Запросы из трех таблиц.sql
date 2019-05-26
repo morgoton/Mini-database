bcabbacf0f0ef80ed3b52ca1110f5a871ec674f4
morgoton <42516049+morgoton@users.noreply.github.com>
2019-05-26 10:45:51 +0300

    Delete Запросы из трех таблиц.sql

--- Запросы из трех таблиц.sql
+++
@@ -1,4 +0,0 @@
--- запрос из трех таблиц
-select cities.city, regions.region, countries.country from cities 
-inner join regions on (regions.id = cities.id_region)
-inner join countries on (countries.id = regions.id_country);
